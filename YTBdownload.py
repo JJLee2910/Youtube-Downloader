a6eaa27213dd700742d9b15c2f9c1de602103af8
Lee Jang Jhin <83653118+JJLee2910@users.noreply.github.com>
2021-11-28 11:28:54 +0800

    Update YTBdownload.py

--- YTBdownload.py
+++ YTBdownload.py
@@ -1,18 +1,21 @@
 from tkinter import *
 from pytube import YouTube
 
+# from tkinter import *
+from pytube import YouTube
+
 # Create a display window
 
 root = Tk()
-root.geometry('500x300')
+root.geometry('600x400')
 root.resizable(0, 0)  # set fix size of the window
 root.title("JJ- Youtube Video Downloader")
 
 # create input for link
 link = StringVar()
 
-Label(root, text='Please enter the link here: ', font='arial 15 bold').place(x=160, y=60)
-link_enter = Entry(root, width=70, textvariable=link).place(x=32, y=90)
+Label(root, text='Please enter the link here: ', font='arial 15 bold').place(x=170, y=70)
+link_enter = Entry(root, width=70, textvariable=link).place(x=50, y=100)
 
 
 # create a download functions
